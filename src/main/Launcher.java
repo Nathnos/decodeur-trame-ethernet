954fd693def7af586be995c88f3b31612fe1df79
villa <villa@192.168.1.47>
2021-12-01 12:15:51 +0100

    Ajout de l’exécutable !

--- src/main/Launcher.java
+++ src/main/Launcher.java
@@ -7,9 +7,8 @@ import decodeur_texte.Decodeur;
 import decodeur_texte.ListeTrames;
 
 public class Launcher {
-
 	public static void main(String[] args) throws FileNotFoundException {
-		String path =  "data/trames_analyse/trame_DNS_2_answers.txt"; // TODO prendre path depuis args
+		String path =  args[0];
 		ListeTrames trames = Decodeur.getListTrames(path);
 		for(int i = 0 ; i < trames.getNbTrames() ; i++) {
 			System.out.println("Trame " + (i+1));
